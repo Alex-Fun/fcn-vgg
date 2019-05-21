9d549b2c08ca767d49fdd10601340ab2d070c8dc
Alex-Fun <408889098@qq.com>
2019-05-21 17:44:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -326,7 +326,8 @@ with sess:
         if gs % 10 == 0:
             gs_b = gs
             logging.debug("Current step c% 10: {0} index:{1}".format(gs, i))
-            gs, loss, summary_string = sess.run([global_step, cross_entropy_loss, merged_summary_op], feed_dict=feed_dict_to_use)
+            # gs, loss, summary_string = sess.run([global_step, cross_entropy_loss, merged_summary_op], feed_dict=feed_dict_to_use)
+            loss, summary_string = sess.run([cross_entropy_loss, merged_summary_op], feed_dict=feed_dict_to_use)
             logging.debug("step {0} Current Loss: {1}  index:{2}".format(gs, loss, i))
             end = time.time()
             logging.debug("[{0:.2f}] imgs/s".format(10 * batch_size / (end - start)))
@@ -334,11 +335,11 @@ with sess:
 
             summary_string_writer.add_summary(summary_string, i)
 
-            if gs_b % 100 == 0:
+            if gs % 100 == 0:
                 save_path = saver.save(sess, os.path.join(log_folder, "model.ckpt"), global_step=gs)
                 logging.debug("Model saved in file: %s" % save_path)
 
-            if gs_b % 200 == 0:
+            if gs % 200 == 0:
                 eval_folder = os.path.join(FLAGS.output_dir, 'eval')
                 if not os.path.exists(eval_folder):
                     os.makedirs(eval_folder)
